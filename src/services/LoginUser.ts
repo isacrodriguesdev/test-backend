4e0292420062f35556c4a2cc6ef13b1d8944dd37
Isac Rodrigues <isacrodriguesdev@protonmail.com>
2025-10-30 18:36:47 -0300

    fix: unauthorizedError

--- src/services/LoginUser.ts
+++ src/services/LoginUser.ts
@@ -1,6 +1,6 @@
 import { Authenticator } from "src/domain/cryptography/Authenticator";
 import { EncryptedPassword } from "src/domain/cryptography/EncryptedPassword";
-import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable, UnauthorizedException } from "@nestjs/common";
 import { UserRepository } from "src/domain/repositories/UserResository";
 
 export interface UserAuthenticationProps {
@@ -10,22 +10,26 @@ export interface UserAuthenticationProps {
 
 @Injectable()
 export class LoginUser {
+  private readonly unauthorizedError = new HttpException({
+    status: HttpStatus.UNAUTHORIZED,
+    error: 'Invalid email or password.',
+  }, HttpStatus.UNAUTHORIZED);
+
   constructor(
     private readonly userRepository: UserRepository,
     private readonly encryptedPassword: EncryptedPassword,
     private readonly authenticator: Authenticator,
   ) { }
 
-
   public async execute(email: string, password: string): Promise<any> {
     const user = await this.userRepository.findByEmail(email);
     if (!user) {
-      throw new UnauthorizedException();
+      throw this.unauthorizedError;
     }
 
     const passwordIsValid = await this.encryptedPassword.compare(password, user.password);
     if (!passwordIsValid) {
-      throw new UnauthorizedException();
+      throw this.unauthorizedError;
     }
     const token = await this.authenticator.sign({
       id: user.id,
